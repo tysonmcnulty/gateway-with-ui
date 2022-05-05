187ece32991c38c41ac20ad3668cc40e1e890abe
asajjad <asajjad@asajjad-a01.vmware.com>
2022-05-05 09:48:55 -0500

    Added router changes

--- angular-router-sample/src/app/app.module.ts
+++ angular-router-sample/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { HeroesListComponent } from './heroes-list/heroes-list.component';
     HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'crisis-list', component: CrisisListComponent},
+      {path: 'heroes-list', component: HeroesListComponent},
+      {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
